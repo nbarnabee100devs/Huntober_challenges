a4c44af2b9c7c0bbcb38033da264a1ad41dd2fcc
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-10-03 22:18:52 +0200

    Initial commit and day 1 solution

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Huntober_challenges
+
 A repo containing my solutions to BlawBlawLaw's series of Huntober challenges
+
+Day 1: https://blog.barbaralaw.me/huntober-2022-day-1
+Day 2: https://blog.barbaralaw.me/huntober-2022-day-2
+Day 3: https://blog.barbaralaw.me/huntober-2022-day-3
---
+++ main.js
@@ -0,0 +1,58 @@
+// Day 1
+
+/* Got to decipher some mixed letters.
+
+'0' <-> 'O'      '5' <-> 'S'
+'1' <-> 'I'      '6' <-> 'G'
+'2' <-> 'Z'      '7' <-> 'L'
+'3' <-> 'E'      '8' <-> 'B'
+'4' <-> 'h'      '9' <-> 'q'
+
+My first thought is to use object keys to sort this out.  I wonder if there's a way to do it bi-directional, but I can't immediately think of one, so I guess I'll do it the "long" way.
+Although actually, since it's 0-9, I could just as easily use an array and rely on index values.  That would also make it easier to be bi-directional, as I could do an arr.includes() check.
+*/
+
+const decoderArrayLetters = ["O", "I", "Z", "E", "h", "S", "G", "L", "B", "q"];
+
+let test = "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770.";
+
+/* when running the test through the function I'm going to write, I'd expect to receive the following result:
+"PRO-TIP #498: IT'S NICE TO SAY HELLO."
+*/
+
+function decodeStringOld(str) {
+  let newStr;
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+    if (decoderArrayLetters.includes(str[i])) {
+      console.log("yes");
+      newStr = str.replace(str[i], String(decoderArrayLetters.indexOf(str[i])));
+    }
+    if (decoderArrayNumbers.includes(str[i])) {
+      newStr = str.replace(str[i], decoderArrayLetters[i]);
+    }
+  }
+  console.log(newStr);
+  return newStr;
+}
+
+// man this is not working at all; what am I doing wrong?
+// oh it's because .replace doesn't actually modify the original string
+// screw it I should just use array stuff
+
+function decodeString(str) {
+  let decodedStr = "";
+  for (let character of str) {
+    if (character === " ") {
+      decodedStr += " ";
+    } else if (decoderArrayLetters.includes(character)) {
+      decodedStr += String(decoderArrayLetters.indexOf(character));
+    } else if (Number(character) >= 0 || Number(character) <= 9) {
+      decodedStr += decoderArrayLetters[Number(character)];
+    } else decodedStr += character;
+  }
+  return decodedStr;
+}
+
+// well this is not the nicest solution by a long shot, and for some reason it was eating all of the whitespace characters
+// I'm sure this is a job for regex
